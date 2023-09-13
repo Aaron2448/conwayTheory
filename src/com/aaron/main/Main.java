e6d4ba8ac3b460a3e8dd0cc01bbb3a09378739e6
4825 <aaron.marinelli@fdmgroup.com>
2023-09-14 02:08:00 +1000

    the conway theory further comments added for clarity

--- src/com/aaron/main/Main.java
+++ src/com/aaron/main/Main.java
@@ -10,8 +10,11 @@ public class Main {
 	    	ArrayList<int[]> deadAdjacentNotChecked = new ArrayList<int[]>();
 	    	String newMatrix = "";
 	    	System.out.println("helloooooooooooooooooo");
+	    	
 	    	// for each alive cell, we first check the number of alive-neighbouring cells, effectively determining each alive-cell's fate
-
+	    	// in summary, we first determine the fate of each alive cell, survivors will be added to the output string
+	    	
+	    	
 	    	for (int x = 0; x < original.length; x++) {
 	    		
 	    		int xCoor = original[x][0];
@@ -90,13 +93,16 @@ public class Main {
 	    		}
 
 	    		// end of inner loop that counts alive-surrounding cells of a single given alive cell
+	    		// now that we have the number of alive-neighbouring cells for the given alive cell, we 
+	    		// decide whether or not to add that cell to the new Matrix
 	    		
 	    		if (aliveNeighbours == 2 || aliveNeighbours == 3) {
 	    			newMatrix = newMatrix + String.valueOf(targetCoor[0]) + "," + String.valueOf(targetCoor[1] + ":");
 	    
 	    		}
 	    	
-	    		// moves onto the next alive cell to count number of neighbouring alive cells
+	    		// moves onto the next alive cell in the list given, to count number of neighbouring alive cells
+	    		// back to the top of this 'for' loop
 	    		
 	    	}	
 	    	
@@ -104,9 +110,20 @@ public class Main {
 	    	System.out.println(deadAdjacentNotChecked.toString());
 	    	System.out.println(deadAdjacentNotChecked.get(0)[0]);
 	    	System.out.println(deadAdjacentNotChecked.get(0)[1]);
-	    	//finished determining the fate of all alive cells provided
 	    	
+	    	//finished determining the fate of all alive cells provided
+	    	// finished adding the ones that will make it to the new matrix string
 	    	// now to go through the list of dead cells that neighbour at least one alive cell
+	    	// in other words, whilst we wwere counting the number of neighbouring alive cells for a given alive cell, 
+	    	// we were also taking note of neighbouring dead cells that will need to be checked for their survival due to 
+	    	// having at least one neghbouring alive cell, I call these "relevant" dead cells, I was keeping a track of relevant dead cells 
+	    	// as I was counting the number of alive cells for a given alive cell.
+	    	// Also note that the relevant-dead-cell list was watching out for duplicates, as some alive cells may share one or more dead cells
+	    	
+	    	
+	    	// now I go through the list or "relevant" dead cells and determine whether they will come alive
+	    	// those that do will be added to the new matrix string
+	    	// then we are done
 	    	
 	    	for (int p = 0; p < deadAdjacentNotChecked.size(); p++) {
 	    		
@@ -179,6 +196,9 @@ public class Main {
 	    		
 	    	}
 	    	
+	    	
+	    	// now we have determined the fate of alive cells and also the fate of all relevant and adjacent dead cells
+	    	// PRINT RESULT
 	    	System.out.println("THIS IS THE NEW MATRIX: " + newMatrix);
 	    	
 	    	
